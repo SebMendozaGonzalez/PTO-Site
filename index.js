be38203eda47877ed6716e3d88c679627d0e9178
Sebastián <s.gonzales@surgicalcapital.com>
2024-09-09 08:31:42 -0500

    Again, switching * for a / in index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/api/hello', (req, res) => {
 });
 
 // Serve the React app
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
