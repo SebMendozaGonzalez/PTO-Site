5c05f1f299f10329fc868883d0b5571df76681be
Sebastian <s.gonzales@surgicalcapital.com>
2024-09-16 10:26:34 -0500

    Simply re-structures the folder system

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,21 @@
+import reportWebVitals from './reportWebVitals';
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
