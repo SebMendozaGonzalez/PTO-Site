4a8f3fc3b4c81f7ca54e2401b911a3dac809b13b
Sebastian <s.gonzales@surgicalcapital.com>
2024-09-16 13:19:59 -0500

    Prepared for connection to database

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,10 +1,14 @@
 // src/components/Footer/Footer.js
 import React from 'react';
 import './Footer.css';
+import white_logo from './white-quantum-long-logo.png'
 
 const Footer = () => {
   return (
     <footer className="footer">
+      <div className="logo-container">
+        <img src={white_logo} alt="Company Logo" className="company-logo" />
+      </div>
       <p>&copy; 2024 Quantum Outsourcing Group. All rights reserved.</p>
     </footer>
   );
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,14 +2,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import logo from './quantum-long-logo.png'
 
 const Header = () => {
   return (
     <header className="header">
       <div className="logo-container">
-        <img 
-        src="https://static.wixstatic.com/media/04c255_d994cd56e81741f2a4d7cc377a3713b1~mv2.png/v1/crop/x_55,y_64,w_1945,h_402/fill/w_264,h_55,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Quantum%20Outsourcing%20Group%20Logo.png" 
-        alt="Company Logo" className="company-logo" />
+        <img src={logo} alt="Company Logo" className="company-logo" />
       </div>
       <nav className="nav-options">
         <Link to="/" className="nav-link">Home</Link>
