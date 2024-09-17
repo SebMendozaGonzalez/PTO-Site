f3b97e29032d741fc07c4a6d12f9e7bb91691669
Sebastian <s.gonzales@surgicalcapital.com>
2024-09-17 11:47:53 -0500

    Cleaned up the remaining vestigies of the react base app, set up icons and metadata for the site, beautified the Home page, and fixed the site header.

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,12 +1,17 @@
 // src/pages/Home/Home.js
+// src/pages/Home/Home.js
+
 import React from 'react';
 import './Home.css';
+import homeImage from '../images/mom-and-son.png'; // Adjust the path as needed
 
 const Home = () => {
   return (
-    <div className="home">
-      <h1>Welcome to the Vacations Portal</h1>
-      <p>Navigate through the portal to manage your vacations.</p>
+    <div className="home" style={{ backgroundImage: `url(${homeImage})`, backgroundSize: 'cover' }}>
+      <div className="home-text">
+        <h1>Welcome to Quantum Vacations</h1>
+        <p>A centralized solution for managing vacation requests.</p>
+      </div>
     </div>
   );
 }
