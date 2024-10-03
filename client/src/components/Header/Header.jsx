86f31a8246234f2f1f525f7bc00507c6b697c9e6
Sebastian <s.gonzales@surgicalcapital.com>
2024-10-03 10:43:07 -0500

    Authentication?

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,34 +1,36 @@
-// src/components/Header/Header.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
-import logo from '../../images/quantum-long-logo.png'
+import logo from '../../images/quantum-long-logo.png';
 
 const Header = () => {
+  
+  const handleLogin = () => {
+    window.location.href = '/.auth/login/aad';  // Redirect to Microsoft Entra login
+  };
+
   return (
     <header className="header">
       <div className="logo-container">
         <img src={logo} alt="Company Logo" className="company-logo" />
       </div>
       <nav className="nav-options">
-
         <Link to="/" className="nav-link">
-        <span>Home</span>
+          <span>Home</span>
         </Link>
         <Link to="/employee-portal" className="nav-link">
-        <span>Employee Portal</span>
+          <span>Employee Portal</span>
         </Link>
         <Link to="/leader-portal" className="nav-link">
-        <span>Leader Portal</span>
+          <span>Leader Portal</span>
         </Link>
-        
+
         <div className='btn-login'>
-          <button className="button">Login</button>
+          <button className="button" onClick={handleLogin}>Login</button>
         </div>
-        
       </nav>
     </header>
   );
-}
+};
 
 export default Header;
