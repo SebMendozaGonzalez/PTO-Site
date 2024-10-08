9a16b6c8ab5d00dc4db2374596e0d82dfed95112
Sebastian <s.gonzales@surgicalcapital.com>
2024-10-08 09:05:47 -0500

    redirect URI corrected

--- client/src/auth/authConfig.js
+++ client/src/auth/authConfig.js
@@ -4,7 +4,7 @@ const msalConfig = {
     auth: {
         clientId: "a564ad6f-c874-40c5-82c4-fbb412756468",
         authority: "https://login.microsoftonline.com/33d1ad6a-c8e7-4be9-bd3b-9942f85502bf",
-        redirectUri: "quantumhr.azurewebsites.net" // Change this to your production URL later
+        redirectUri: "https://quantumhr.azurewebsites.net/.auth/login/aad/callback" // Change this to your production URL later
     },
     cache: {
         cacheLocation: "localStorage", // This can also be 'sessionStorage'
