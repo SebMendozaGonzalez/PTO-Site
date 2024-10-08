c98ede0be04efd00181e5daf41bcfcff76f7ea46
Sebastian <s.gonzales@surgicalcapital.com>
2024-10-08 08:56:31 -0500

    debugging

--- client/src/components/WelcomeEmployees/WelcomeEmployees.jsx
+++ client/src/components/WelcomeEmployees/WelcomeEmployees.jsx
@@ -4,20 +4,22 @@ import { useMsal } from '@azure/msal-react';
 import './WelcomeEmployees.css';
 
 function WelcomeEmployees() {
-    const { accounts } = useMsal();
+  const { accounts } = useMsal();
 
-    return (
-        <div className='flexColStart paddings innerWidth welcomeEmployees'>
-            <h1 className='fonts-secondary' style={{ fontWeight: "700" }}>
-                Welcome, {accounts.length > 0 ? accounts[0].name : 'User'}!
-            </h1>
-            <div className="flexStart paragraph">
-                <p className="fonts-regular" style={{ fontSize: "1.4rem" }}>
-                    Here, you will be able to manage your vacation requests and view your remaining vacation days.
-                </p>
-            </div>
-        </div>
-    );
+  console.log(accounts); // Add this line for debugging
+
+  return (
+      <div className='flexColStart paddings innerWidth welcomeEmployees'>
+          <h1 className='fonts-secondary' style={{ fontWeight: "700" }}>
+              Welcome, {accounts.length > 0 ? accounts[0].name : 'User'}!
+          </h1>
+          <div className="flexStart paragraph">
+              <p className="fonts-regular" style={{ fontSize: "1.4rem" }}>
+                  Here, you will be able to manage your vacation requests and view your remaining vacation days.
+              </p>
+          </div>
+      </div>
+  );
 }
 
 export default WelcomeEmployees;
