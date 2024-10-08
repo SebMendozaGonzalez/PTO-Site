2bab9c48fec65650f45b89a700cc375f3785f232
Sebastian <s.gonzales@surgicalcapital.com>
2024-10-08 08:51:45 -0500

    User displaying problem solved

--- client/src/auth/authConfig.js
+++ client/src/auth/authConfig.js
@@ -1,11 +1,16 @@
 import { PublicClientApplication } from '@azure/msal-browser';
 
 const msalConfig = {
-  auth: {
-    clientId: "a564ad6f-c874-40c5-82c4-fbb412756468",
-    authority: "https://login.microsoftonline.com/33d1ad6a-c8e7-4be9-bd3b-9942f85502bf",
-    redirectUri: "quantumhr.azurewebsites.net" // Change this to your production URL later
-  }
+    auth: {
+        clientId: "a564ad6f-c874-40c5-82c4-fbb412756468",
+        authority: "https://login.microsoftonline.com/33d1ad6a-c8e7-4be9-bd3b-9942f85502bf",
+        redirectUri: "quantumhr.azurewebsites.net" // Change this to your production URL later
+    },
+    cache: {
+        cacheLocation: "localStorage", // This can also be 'sessionStorage'
+        storeAuthStateInCookie: true, // Set this to true if you're having issues on IE11 or Edge
+    },
+    scopes: ["openid", "profile"] // Include this line
 };
 
 const msalInstance = new PublicClientApplication(msalConfig);
