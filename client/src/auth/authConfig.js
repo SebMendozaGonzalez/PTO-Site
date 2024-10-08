a03810df0b9c00c88a36115ef428b56fb18a996f
Sebastian <s.gonzales@surgicalcapital.com>
2024-10-08 08:10:53 -0500

    implemented personalization based on the login

---
+++ client/src/auth/authConfig.js
@@ -0,0 +1,13 @@
+import { PublicClientApplication } from '@azure/msal-browser';
+
+const msalConfig = {
+  auth: {
+    clientId: "a564ad6f-c874-40c5-82c4-fbb412756468",
+    authority: "https://login.microsoftonline.com/33d1ad6a-c8e7-4be9-bd3b-9942f85502bf",
+    redirectUri: "quantumhr.azurewebsites.net" // Change this to your production URL later
+  }
+};
+
+const msalInstance = new PublicClientApplication(msalConfig);
+
+export default msalInstance;
--- client/src/components/WelcomeEmployees/WelcomeEmployees.jsx
+++ client/src/components/WelcomeEmployees/WelcomeEmployees.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import './WelcomeEmployees.css'
+import React from 'react';
+import { useMsal } from '@azure/msal-react';
+import './WelcomeEmployees.css';
 
 function WelcomeEmployees() {
+  const { accounts } = useMsal();
+  
+  // Get the name of the signed-in user
+  const userName = accounts[0]?.name || "User";
+
   return (
     <div className='flexColStart paddings innerWidth welcomeEmployees'>
-
-        <h1 className='fonts-secondary' style={{fontWeight: "700"}}>Welcome, Sebastián!</h1>
-        
-        <div className = "flexStart paragraph">
-            <p className="fonts-regular" style={{fontSize: "1.4rem"}}>
-                Here, you will be able to manage your vacation requests and view your remaining vacation days.
-            </p>
-        </div>
-        
-
+      <h1 className='fonts-secondary' style={{fontWeight: "700"}}>Welcome, {userName}!</h1>
+      <div className="flexStart paragraph">
+        <p className="fonts-regular" style={{fontSize: "1.4rem"}}>
+          Here, you will be able to manage your vacation requests and view your remaining vacation days.
+        </p>
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default WelcomeEmployees
+export default WelcomeEmployees;
