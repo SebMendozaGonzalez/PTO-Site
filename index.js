f119319ceca194e9bf421713041ab1b7c390f920
MenGonz <semendozag@unal.edu.co>
2024-10-09 19:23:50 -0500

    added the error handling for the middleware

--- index.js
+++ index.js
@@ -54,6 +54,12 @@ app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
