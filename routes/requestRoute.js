b4e6aee39b466d81cc2318d2f805a0e7d187c065
Sebastian <s.gonzales@surgicalcapital.com>
2024-10-18 15:59:17 -0500

    ultimo del día de trabajo

--- routes/requestRoute.js
+++ routes/requestRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { connectToDatabase } = require('../db/dbConfig');  // Your SQL connection pool
+const { connectToDatabase } = require('../db/dbConfig');
 
 // Route to handle vacation requests
 router.post('/', async (req, res) => {
@@ -8,7 +8,7 @@ router.post('/', async (req, res) => {
     const pool = await connectToDatabase();
     const { type, start_date, end_date, explanation, employee_id, is_exception } = req.body;
 
-    // Log the incoming data
+
     console.log('Inserting request with the following data:', {
         type,
         start_date,
@@ -19,24 +19,22 @@ router.post('/', async (req, res) => {
     });
 
     try {
-        // Insert the new vacation request
         await pool.query(
             `INSERT INTO request (type, start_date, end_date, request_date, explanation, employee_id, is_exception)
              VALUES ($1, $2, $3, CURRENT_DATE, $4, $5, $6)`,
             [type, start_date, end_date, explanation, employee_id, is_exception]
         );
         
-        // Retrieve the last inserted row for that employee
+
         const result = await pool.query(
             `SELECT * FROM request WHERE employee_id = $1 ORDER BY request_date DESC LIMIT 1`,
             [employee_id]
         );
         
-        // Send the inserted row back in the response
+
         res.status(201).json(result.rows[0]);
     } catch (err) {
         console.error('Error submitting request:', err.message);
-        // Ensure the response is always JSON formatted
         res.status(500).json({ message: 'Error submitting request', error: err.message || 'Internal Server Error' });
     }
 });
