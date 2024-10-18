126bce5597742f805a54901435d26f74469e5324
Sebastian <s.gonzales@surgicalcapital.com>
2024-10-18 13:26:38 -0500

    again

--- routes/requestRoute.js
+++ routes/requestRoute.js
@@ -10,7 +10,7 @@ router.post('/', async (req, res) => {
     try {
         // Perform the INSERT operation without RETURNING
         await pool.query(
-            `INSERT INTO request (type_of_to, start_date, end_date, request_date, explanation, employee_id, is_exception)
+            `INSERT INTO request (type, start_date, end_date, request_date, explanation, employee_id, is_exception)
              VALUES ($1, $2, $3, CURRENT_DATE, $4, $5, $6)`,
             [type_of_to, start_date, end_date, explanation, employee_id, is_exception]
         );
