7fdf863715ef221b639406cc97e37e2f1a144c85
Sebastian <s.gonzales@surgicalcapital.com>
2024-10-23 12:00:37 -0500

    solved a bunch of ugly things

--- client/src/components/RequestView/RequestView.jsx
+++ client/src/components/RequestView/RequestView.jsx
@@ -8,46 +8,46 @@ function RequestView({ requestDetails, onClose }) {
         <div className='request-popup'>
             <div className='popup-content'>
                 <button className='close-btn' onClick={onClose}>✕</button>
-                <div className='paddings flexColCenter'>
-                    <div className='first paddings flexCenter'>
-                        <div className='paddings flexStart requester-info'>
+                <div className='paddings flexColCenter innerWidth'>
+                    <div className='first padding flexCenter innerWidth'>
+                        <div className='paddings flexStart left  requester-info'>
                             <div className='flexColCenter'>
-                                <span className='fonts-primary'>{requestDetails.name}</span>
-                                <span className='fonts-secondary'>{requestDetails.employee_id}</span>
+                                <span className='f1'>{requestDetails.name}</span>
+                                <span className='f2'>{requestDetails.employee_id}</span>
                             </div>
                         </div>
-                        <div className='paddings flexEnd dates-and-type'>
+                        <div className='paddings flexStart right dates-and-type'>
                             <div className='flexColCenter'>
-                                <span className='fonts-primary'>{requestDetails.type}</span>
-                                <span className='fonts-secondary'>
+                                <span className='f1'>{requestDetails.type}</span>
+                                <span className='f2'>
                                     <strong>from: </strong>{new Date(requestDetails.start_date).toLocaleDateString('en-US')}
                                 </span>
-                                <span className='fonts-secondary'>
+                                <span className='f2'>
                                     <strong>to: </strong>{new Date(requestDetails.end_date).toLocaleDateString('en-US')}
                                 </span>
                             </div>
                         </div>
                     </div>
-                    <div className='second paddings flexColStart'>
-                        <span className='fonts-regular'>
+                    <div className='second padding flexColStart innerWidth'>
+                        <span className='f3'>
                             <strong>Requested on </strong>{new Date(requestDetails.request_date).toLocaleDateString('en-US')}
                         </span>
                         {requestDetails.accepted && (
-                            <span className='fonts-regular'>
+                            <span className='f3'>
                                 <strong>Accepted on </strong>{new Date(requestDetails.decision_date).toLocaleDateString('en-US')}
                             </span>
                         )}
                         {requestDetails.taken && (
-                            <span className='fonts-regular-italic'>This time off was already taken</span>
+                            <span className='f3-italic'>This time off was already taken</span>
                         )}
                     </div>
-                    <div className='third paddings flexCenter'>
-                        <div className='flexColStart'>
-                            <span className='fonts-primary'>Justification</span>
-                            <p className='fonts-regular'>{requestDetails.explanation}</p>
+                    <div className='third paddings flexCenter innerWidth'>
+                        <div className='flexColStart left'>
+                            <span className='f1'>Justification</span>
+                            <p className='f3'>{requestDetails.explanation}</p>
                         </div>
-                        <div className='flexColEnd'>
-                            <span className='fonts-regular'>Docs</span>
+                        <div className='flexColStart right'>
+                            <span className='f0'>Docs</span>
                         </div>
                     </div>
                 </div>
