e4805a60d08161e0acaa2d41446de6e5254fc60b
Sebastian <s.gonzales@surgicalcapital.com>
2024-11-05 15:23:10 -0500

    paragraph

--- client/src/components/WelcomeHRManagers/WelcomeHRManagers.jsx
+++ client/src/components/WelcomeHRManagers/WelcomeHRManagers.jsx
@@ -8,21 +8,21 @@ function WelcomeHRManagers() {
 
     return (
         <div className='contain paddings innerWidth welcomeEmployees'>
-            <div className='left'>
+            <div className='flexColStart left'>
                 <h1 className='fonts-secondary' style={{ fontWeight: "700" }}>
                     Welcome, {accounts.length > 0 ? accounts[0].name : 'HR Manager'}!
                 </h1>
-                <div className="flexStart paragraph">
+                <div className="paragraph">
                     <p className="fonts-regular" style={{ fontSize: "1.4rem" }}>
                         Here, you will be able to manage everyone's vacation requests, view their remaining days, upload their disabilities, add new collaborators, take old ones out of the roster, and more.
                     </p>
                 </div>
             </div>
-            <div className='right'>
+            <div className='flexColStart right'>
                 <h1 className='fonts-secondary' style={{ fontWeight: "700" }}>
                     Manage Collaborators
                 </h1>
-                <div className="flexStart paragraph">
+                <div className="paragraph">
                     <p className="fonts-regular" style={{ fontSize: "1.4rem" }}>
                         Here, you will be able to review, manage and update the company's roster. You could add new employees or remove the ones that are no longer with us.
                     </p>
