44b07a99f233607d87ccb3aece4e43de1da7844d
Sebastian <s.gonzales@surgicalcapital.com>
2024-11-05 16:09:54 -0500

    added buttons to manage things out

--- client/src/components/WelcomeHRManagers/WelcomeHRManagers.jsx
+++ client/src/components/WelcomeHRManagers/WelcomeHRManagers.jsx
@@ -7,27 +7,50 @@ function WelcomeHRManagers() {
     const { accounts } = useMsal();
 
     return (
-        <div className='contain paddings innerWidth welcomeEmployees'>
-            <div className='column left'>
-                <h1 className='fonts-secondary' style={{ fontWeight: "700" }}>
-                    Welcome, {accounts.length > 0 ? accounts[0].name : 'HR Manager'}!
-                </h1>
-                <div className="paragraph">
-                    <p className="fonts-regular" style={{ fontSize: "1.4rem" }}>
-                        Here, you will be able to manage everyone's vacation requests, view their remaining days, upload their disabilities, add new collaborators, take old ones out of the roster, and more.
-                    </p>
+        <div className='flexColStart paddings innerWidth welcome-hr'>
+            <div className='row innerWidth paddings'>
+                <div className='column left'>
+                    <h1 className='fonts-secondary' style={{ fontWeight: "700" }}>
+                        Welcome, {accounts.length > 0 ? accounts[0].name : 'HR Manager'}!
+                    </h1>
+                    <div className="paragraph">
+                        <p className="fonts-regular" style={{ fontSize: "1.4rem" }}>
+                            Here, you will be able to manage everyone's vacation requests, view their remaining days, upload their disabilities, add new collaborators, take old ones out of the roster, and more.
+                        </p>
+                    </div>
                 </div>
             </div>
-            <div className='column right'>
-                <h1 className='fonts-secondary' style={{ fontWeight: "700" }}>
-                    Manage Collaborators
-                </h1>
-                <div className="paragraph">
-                    <p className="fonts-regular" style={{ fontSize: "1.4rem" }}>
-                        Here, you will be able to review, manage and update the company's roster. You could add new employees or remove the ones that are no longer with us.
-                    </p>
+            <div className='row innerWidth paddings'>
+                <div className='column left'>
+                    <h1 className='fonts-secondary' style={{ fontWeight: "700" }}>
+                        Manage Time Off Requests
+                    </h1>
+                    <div className="paragraph">
+                        <p className="fonts-regular" style={{ fontSize: "1.4rem" }}>
+                            Here, you will be able to review decide over the requests from everyone in the company.
+                        </p>
+                    </div>
+                </div>
+                <div className='paddings right'>
+                    <button className='button'>Go</button>
                 </div>
             </div>
+            <div className='row innerWidth paddings'>
+                <div className='column left'>
+                    <h1 className='fonts-secondary' style={{ fontWeight: "700" }}>
+                        Manage Collaborators
+                    </h1>
+                    <div className="paragraph">
+                        <p className="fonts-regular" style={{ fontSize: "1.4rem" }}>
+                            Here, you will be able to review, manage and update the company's roster. You could add new employees or remove the ones that are no longer with us.
+                        </p>
+                    </div>
+                </div>
+                <div className='paddings right'>
+                    <button className='button'>Go</button>
+                </div>
+            </div>
+
         </div>
     );
 }
