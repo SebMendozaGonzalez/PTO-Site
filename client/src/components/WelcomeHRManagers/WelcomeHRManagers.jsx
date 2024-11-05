b39eeff809b34e7f1894cbbd55062aaa0d183f5e
Sebastian <s.gonzales@surgicalcapital.com>
2024-11-05 15:03:02 -0500

    Manage collaborators

--- client/src/components/WelcomeHRManagers/WelcomeHRManagers.jsx
+++ client/src/components/WelcomeHRManagers/WelcomeHRManagers.jsx
@@ -4,20 +4,32 @@ import { useMsal } from '@azure/msal-react';
 import './WelcomeHRManagers.css';
 
 function WelcomeHRManagers() {
-  const { accounts } = useMsal();
+    const { accounts } = useMsal();
 
-  return (
-      <div className='flexColStart paddings innerWidth welcomeEmployees'>
-          <h1 className='fonts-secondary' style={{ fontWeight: "700" }}>
-              Welcome, {accounts.length > 0 ? accounts[0].name : 'HR Manager'}!
-          </h1>
-          <div className="flexStart paragraph">
-              <p className="fonts-regular" style={{ fontSize: "1.4rem" }}>
-                  Here, you will be able to manage everyone's vacation requests, view their remaining days, upload their disabilities, add new collaborators, take old ones out of the roster, and more.
-              </p>
-          </div>
-      </div>
-  );
+    return (
+        <div className='flexColStart paddings innerWidth welcomeEmployees'>
+            <div className='flexColStart'>
+                <h1 className='fonts-secondary' style={{ fontWeight: "700" }}>
+                    Welcome, {accounts.length > 0 ? accounts[0].name : 'HR Manager'}!
+                </h1>
+                <div className="flexStart paragraph">
+                    <p className="fonts-regular" style={{ fontSize: "1.4rem" }}>
+                        Here, you will be able to manage everyone's vacation requests, view their remaining days, upload their disabilities, add new collaborators, take old ones out of the roster, and more.
+                    </p>
+                </div>
+            </div>
+            <div className='flexColEnd'>
+                <h1 className='fonts-secondary' style={{ fontWeight: "700" }}>
+                    Manage Collaborators
+                </h1>
+                <div className="flexStart paragraph">
+                    <p className="fonts-regular" style={{ fontSize: "1.4rem" }}>
+                        Here, you will be able to review, manage and update the company's roster. You could add new employees or remove the ones that are no longer with us.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default WelcomeHRManagers;
