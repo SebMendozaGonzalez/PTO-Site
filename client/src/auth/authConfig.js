6a689ac8a0ed4eefedc94d57ed8946fdeb427aa0
Sebastián <dev1@surgicalcapital.com>
2024-12-26 13:27:17 -0500

    RedirectUri modification

--- client/src/auth/authConfig.js
+++ client/src/auth/authConfig.js
@@ -5,7 +5,8 @@ export const msalConfig = {
   auth: {
     clientId: "a564ad6f-c874-40c5-82c4-fbb412756468",
     authority: "https://login.microsoftonline.com/33d1ad6a-c8e7-4be9-bd3b-9942f85502bf",
-    redirectUri: "https://quantumhr.azurewebsites.net/auth/callback"
+    redirectUri: "https://quantumhr.azurewebsites.net/.auth/login/aad/callback"
+
   },
   cache: {
     cacheLocation: "sessionStorage",
