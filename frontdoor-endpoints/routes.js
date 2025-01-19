89abeeced245af337669ccb588f15304a4030a8d
MenGonz <semendozag@unal.edu.co>
2025-01-19 15:46:52 -0500

    routes marked

--- frontdoor-endpoints/routes.js
+++ frontdoor-endpoints/routes.js
@@ -3,17 +3,18 @@ const apiProxy = require('./apiProxy');
 
 const router = express.Router();
 
-// Use the proxy middleware for all /api/ routes
-const endpoints = [
-    '/employee',
-    '/request',
-    '/liquidation-request',
-    '/vacations-info',
-    '/email_id',
-    '/employees-by-leader',
-];
-endpoints.forEach((endpoint) => {
-    router.use(endpoint, apiProxy());
+// Debug log for incoming requests
+router.use((req, res, next) => {
+    console.log('[Frontdoor Router] Incoming request to:', req.originalUrl);
+    next();
 });
+
+// Use the proxy middleware for all /api/ routes
+router.use('/employee', apiProxy());
+router.use('/request', apiProxy());
+router.use('/liquidation-request', apiProxy());
+router.use('/vacations-info', apiProxy());
+router.use('/email_id', apiProxy());
+router.use('/employees-by-leader', apiProxy());
 
 module.exports = router;
