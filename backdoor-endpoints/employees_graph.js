6b56fdd802a2977827d997808c87aa9979a7c436
Sebastián <dev1@surgicalcapital.com>
2025-01-28 16:09:56 -0500

    syntax

--- backdoor-endpoints/employees_graph.js
+++ backdoor-endpoints/employees_graph.js
@@ -16,7 +16,7 @@ router.get('/:leader_email', async (req, res) => {
                 WITH RecursiveEmployees AS (
                     SELECT *
                     FROM dbo.roster
-                    WHERE leader_email = @leader_email AND email_surgical != 'gary@surgicalcapital.com'
+                    WHERE (leader_email = @leader_email) AND (email_surgical != leader_email)
                     UNION ALL
                     SELECT r.*
                     FROM dbo.roster r
