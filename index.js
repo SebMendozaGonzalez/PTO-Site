a352da2eee7ec241412c1fd134284b8e8353f520
Sebastián <dev1@surgicalcapital.com>
2025-03-17 15:27:43 -0500

    changed the cors configuration

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const port = process.env.PORT || 8080;
 // Middleware: Handle CORS
 app.use(
   cors({
-    origin: ['https://quantumhr-quantumh-testing.azurewebsites.net','https://quantumhr.azurewebsites.net'],
+    origin: ['https://quantumhr-quantumh-testing.azurewebsites.net'],
     credentials: true,
   })
 );
